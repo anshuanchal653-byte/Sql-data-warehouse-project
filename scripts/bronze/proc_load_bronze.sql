dc3bef9c8c37afdfaec98338d4eb355966e412ff
Anshu Anchal <anshuanchal653@gmail.com>
2026-06-28 00:26:51 +0530

    Update proc_load_bronze.sql

--- scripts/bronze/proc_load_bronze.sql
+++ scripts/bronze/proc_load_bronze.sql
@@ -98,3 +98,44 @@ SET @start_time = GETDATE();
 PRINT '>> Truncating Table: bronze.erp_cust_az12';
 TRUNCATE TABLE bronze.erp_cust_az12;
 PRINT 'Intresting Data Into: bronze.erp_cust_az12';
+BULK INSERT bronze.erp_cust_az12
+FROM 'C:\sql\dwh_project\datasets\source_erp\cust_az12.csv'
+WITH (
+   FIRSTROW 2,
+   FIELDTERMINATOR = ', ',
+   TABLOCK
+);
+SET @end_time = GETDATE();
+PRINT '>> Load Duration: ' + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+PRINT '>> ------------';
+
+SET @start_time = GETDATE();
+PRINT '>> Truncating Table: bronze.erp_px_cat_g1v2';
+TRUNCATE TABLE bronze.erp_px_cat_g1v2;
+PRINT '>> Inserting Data Into: bronze.erp_px_cat_g1v2';
+BULK INSERT bronze.erp_px_cat_g1v2
+FROM 'C:\sql\dwh_project\datasets\source_erp\px_cat_g1v2.csv'
+WITH (
+  FIRSTROW 2,
+  FIELDTERMINATOR = ', ',
+  TABLOCK
+);
+SET @end_time = GETDATE();
+PRINT >> Load Duration: + CAST (DATEDIFF(second, @start_time, @end_time) AS NVARCHAR) + ' seconds';
+PRINT '>> ------------';
+
+SET @batch_end_time = GETDATE();
+PRINT '================================='
+PRINT 'Loading Bronze Layer is Completed';
+   PRINT' Total Load Duration: ' + CAST (DATEDIFF(SECOND, @batch_start_time, @batch_end_time) AS NVARCHAR) + seconds';
+PRINT '=================================='
+END TRY
+BEGIN CATCH
+  PRINT '=================================='
+  PRINT 'ERROR OCCURED DURING LOADING BRONZE LAYER'
+  PRINT 'Error Message + ERROR_MESSAGE();
+  PRINT 'Error Message + CAST (ERROR_NUMBER() AS NVARCHAR);
+  PRINT 'Error Message + CAST (ERROR_STATE() AS NVARCHAR);
+  PRINT '==================================='
+END CATCH
+END
